bd5018949bc2d650e3b6a9ba0458a97ba834d505
BadarAhmedSheikh <badr_merchant@yahoo.com>
2023-03-16 03:36:30 +0500

    add two functions

--- index.ts
+++ index.ts
@@ -7,3 +7,12 @@ function minus(a:number,b:number):number{
     return a-b;
 }
 
+
+function quadratic(a:number,b:number,c:number):number{
+    return b-4*a*b*c;
+}
+
+function line(a:number,b:number):number{
+    return (a-b)+(a+b);
+}
+
